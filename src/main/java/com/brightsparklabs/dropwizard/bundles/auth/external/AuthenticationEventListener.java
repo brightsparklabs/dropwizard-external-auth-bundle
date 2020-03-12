bdcdce85332f041118c0df9dacb68b960fa4e152
Thomas Anderson <thomas.anderson@brightsparklabs.com>
2020-03-13 10:48:54 +1100

    TERA-185: Change back to interface, and have an 'empty' abstract implementation so that methods can be optionally overridden

--- src/main/java/com/brightsparklabs/dropwizard/bundles/auth/external/AuthenticationEventListener.java
+++ src/main/java/com/brightsparklabs/dropwizard/bundles/auth/external/AuthenticationEventListener.java
@@ -8,11 +8,11 @@ package com.brightsparklabs.dropwizard.bundles.auth.external;
 import io.dropwizard.auth.AuthenticationException;
 
 /**
- * Abstract class for listeners to authentication events.
+ * Interface for listeners to authentication events.
  *
  * @author brightSPARK Labs
  */
-public abstract class AuthenticationEventListener {
+public interface AuthenticationEventListener {
     // TODO: Clean interface with abstract impl 'AbstractAuthenticationEventListener'
 
     /**
@@ -20,18 +20,18 @@ public abstract class AuthenticationEventListener {
      *
      * @param authenticatedUser the successfully-authenticated user
      */
-    void onAuthenticationSuccess(InternalUser authenticatedUser){}
+    void onAuthenticationSuccess(InternalUser authenticatedUser);
 
     /**
      * Listener function called on authentication failure - denied access
      */
-    void onAuthenticationDenied(AuthenticationDeniedException authDeniedException){}
+    void onAuthenticationDenied(AuthenticationDeniedException authDeniedException);
 
     /**
      * Listener function called on authentication failure - invalid authentication request
      *
      * @param authException the authentication exception that was thrown
      */
-    void onAuthenticationError(AuthenticationException authException){}
+    void onAuthenticationError(AuthenticationException authException);
 
 }
